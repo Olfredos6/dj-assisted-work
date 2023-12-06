1500927ee892197ef67471a132bc80a4614feba6
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2023-12-06 11:23:25 +0200

    update tests v1

--- tests.py
+++ tests.py
@@ -1,9 +1,65 @@
+import datetime
+
+import factory
 from django.test import TestCase
 
 from . import models
 
 
-class PublisherTestCase(TestCase):
+class PublisherFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Publisher
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Category
+
+
+class NoCategoryBookFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Book
+
+    publisher = factory.SubFactory(PublisherFactory)
+    date_published = factory.Faker("date_time")
+    price = factory.Faker("pydecimal", left_digits=3, right_digits=2, positive=True)
+    rating = factory.Faker("pydecimal", left_digits=1, right_digits=2, positive=True)
+
+
+class PublisherModelTestCase(TestCase):
     def test_can_create_publisher(self):
-        publisher = models.Publisher.objects.create(name="Publisher 1")
+        publisher = PublisherFactory.create()
         self.assertQuerySetEqual(models.Publisher.objects.all(), [publisher])
+
+    def test_model_str_method_has_instance_name_in_it(self):
+        publisher = PublisherFactory.build()
+        publisher.name = "First Publisher"
+        self.assertIn(publisher.name, str(publisher))
+
+
+class CategoryModelTestCase(TestCase):
+    def test_can_create_category(self):
+        category = CategoryFactory.create()
+        self.assertQuerySetEqual(models.Category.objects.all(), [category])
+
+    def test_model_str_method_has_instance_name_in_it(self):
+        category = CategoryFactory.build()
+        category.name = "Some Category"
+        self.assertIn(category.name, str(category))
+
+
+class BookModelTestCase(TestCase):
+    def test_can_create_book(self):
+        book = NoCategoryBookFactory.create()
+        self.assertQuerySetEqual(models.Book.objects.all(), [book])
+
+    def test_can_add_categories_to_book(self):
+        book = NoCategoryBookFactory.create()
+        categories = CategoryFactory.create_batch(5)
+        book.categories.add(*categories)
+        self.assertQuerySetEqual(book.categories.all().order_by("id"), categories)
+
+    def test_model_str_method_has_instance_name_in_it(self):
+        book = NoCategoryBookFactory.build()
+        book.title = "Some Book"
+        self.assertIn(book.title, str(book))
